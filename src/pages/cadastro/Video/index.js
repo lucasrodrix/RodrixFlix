ec537e02baa20d075e41cf8a266f63e133e1cddd
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-08-01 19:33:41 -0300

    Criado template para Cabeçalho e Rodape

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefaut';
 
 function CadastroVideo(){
     return(
-      <div>
-        Página de Cadastro de Vídeo
-      </div>
+      <PageDefault>
+        <h1>Cadastro de Vídeo</h1>
+
+        <Link to='/cadastro/categoria'>
+            Cadastrar Categoria
+        </Link>
+      </PageDefault>
     )
   }
 
