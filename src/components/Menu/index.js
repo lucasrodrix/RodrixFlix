a85fda5ce8bef7424f6c76f2bbdbbe5a3b5c13cd
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-08-01 18:53:31 -0300

    Criaçao dos componentes das rotas e refatorado arquivo App.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/imgs/logo.png';
 import './Menu.css';
 // import ButtonLink from '../components/ButtonLink';
@@ -7,10 +8,10 @@ import Button from '../Button';
 function Menu(){
     return(
         <nav className='Menu'>
-            <a href="/">
+            <Link to="/">
                 <img className='Logo' src={Logo} alt="rodrixflix logo"/>
-            </a>
-            <Button as="a" href="/" className="ButtonLink">
+            </Link>
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo Vídeo
             </Button>
         </nav>
